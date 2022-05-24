c39302984ebac366bc4d090e7dc0a64d0943be5e
Sergio Lagos Vergara <sergio.lagos@mail.udp.cl>
2022-05-23 22:25:03 -0400

    solotodo login

---
+++ bash/solotodo/login.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#This script takes the credentials.csv file created with the create_accounts scrips.
+green='\033[0;32m'
+nocolor='\033[0m'
+yellow='\033[0;33m'
+
+input="credentials.csv"
+while IFS=, read -r mail password; do
+    curl -s -o /dev/null -H 'Content-Type: application/json' https://publicapi.solotodo.com/rest-auth/registration/ -d "$(
+        cat <<EOF
+    {
+            "email": "$mail",
+            "password": "$password"
+    }
+EOF
+    )"
+    echo -e "${green}ACCOUNT LOGIN SUCCESSFUL.${nocolor} ${yellow}MAIL: "${mail}${nocolor}
+done <$input
